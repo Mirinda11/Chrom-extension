ddb5ca43e1ae5b57606b859c584cc63b4c2eaaf2
Mirinda <iryna.dmytrash@gmail.com>
2025-05-29 11:30:56 +0200

    dger

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl).value;
+});
+
+function sabmitted(){
+var items = [];
+boxvalue = document.getElementById('input-el').value;
+items.push(inputElvalue);
+console.log(items);
+}
